32ab231e9ec19670212d7c8238bdeb9daa5f3dfe
fran1688 <fcastillo1688@gmail.com>
2017-09-06 18:03:23 -0300

    the name of the variable

--- prueba.rb
+++ prueba.rb
@@ -1,14 +1,9 @@
-#Opción 1: Debe generar un archivo con el nombre de cada alumno y el
-#promedio de sus notas.
-#Se puede leer el archivo completo o ir leyendo y procesando
-#por línea, ambas opciones son válidas.
-
 def show_add_students
   file = File.open('students.csv','r').readlines
-  new_lines_stud = []
-  file.each { |line| new_lines_stud.push(line.split(', ').map(&:chomp)) }
+  lines_stud = []
+  file.each { |line| lines_stud.push(line.split(', ').map(&:chomp)) }
 
-  new_lines_stud.each do |notes|
+  lines_stud.each do |notes|
     name = notes.shift
     average = get_sum_pro(notes)
     File.open('estudiantes.txt', 'a'){ |file| file.puts "#{name} tiene un promedio #{average}" }
@@ -16,6 +11,7 @@ def show_add_students
   puts "Se Genero el el archivo"
 end
 
+#takes the average of the students
 def show_non_attendance
 non_attendance = File.open('students.csv', 'r').readlines
   non_attendance.map do |note|
@@ -24,7 +20,7 @@ non_attendance = File.open('students.csv', 'r').readlines
     name = new_lines_stud.shift
     assistance = new_lines_stud.select.count { |ele| ele == 'A' }
     
-      puts "El Estudiante #{name} tuvo #{assistance} inasistencia."
+      puts "El Estudiante #{name} tuvo #{assistance} inasistencia." if assistance > 0
     
   end
 end
@@ -58,7 +54,7 @@ option = 0
 while option != 4
   puts
   puts '1. Generar archivo con los nombre de los estudiantes y el promedio de notas'
-  puts '2. Cantidad de inasistencias totales'
+  puts '2. Cantidad de inasistencias de los Estudiantes'
   puts '3. Los nombres de los alumnos aprobados'
   puts '4. Salir'  
 
